79405036813da8f591e7e87a6bba3d03de5cc00f
Prasanth Raja Sekaran <prasanth.dps@gmail.com>
2018-08-25 15:49:23 -0400

    Update Format a string of names like 'Bart, Lisa & Maggie'.py

--- Kata/Format a string of names like 'Bart, Lisa & Maggie'.py
+++ Kata/Format a string of names like 'Bart, Lisa & Maggie'.py
@@ -1,20 +1,20 @@
 def namelist(names):
     name = []
-    print(len(names))
-    #your code here
+
     if len(names)>2:
         for items in range(0,len(names)-1):
             name.append(names[items]["name"])
-            print(items, names[items]["name"])
-        print(name)
         a = ", ".join(name)
         return a + " & " + names[len(names)-1]["name"]
+    
     elif len(names)==2:
         for items in names:
             name.append(items["name"])
         return " & ".join(name)
+    
     elif len(names)==1:
         return names[0]["name"]
+    
     else:
         return ''
         
