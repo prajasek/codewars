5e257bef67a64060409f071905107d08f5dffa9d
Prasanth Raja Sekaran <prasanth.dps@gmail.com>
2018-08-25 15:39:19 -0400

    Update Growth of a Population.py

--- Kata/Growth of a Population.py
+++ Kata/Growth of a Population.py
@@ -1,5 +1,13 @@
 def nb_year(p0, percent, aug, p):
-    year=0
+    # nb_year returns the number of years needed to reach population 'p' 
+    # starting from population 'p0'
+    # 
+    # p0 - Initial population
+    # percent - percentage growth in population per year
+    # aug  - increase/decrease in population per year (added or subtracted) 
+    # p - Threshold of population to reach
+    
+    year=0            # keep count of number of years
     while p0<p:
         p0 = p0 + p0*percent/100 + aug
         year=year+1
