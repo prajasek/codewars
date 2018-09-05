0876cd05ae4beed124e851aac80d31bffc89f94a
Prasanth Raja Sekaran <prasanth.dps@gmail.com>
2018-09-04 20:43:09 -0400

    Update Check Isogram.py

--- Kata/Check Isogram.py
+++ Kata/Check Isogram.py
@@ -1,6 +1,18 @@
 def is_isogram(string):
-
-    count  = [string.lower().count(x) for x in string.lower()]
+    
+    # Check if given string is an Isogram: 
+    # If each letter of the string is unique
+    # (not repeated)
+    #
+    lowercase_string = string.lower()    
+    count  = [lowercase_string.count(x) for x in lowercase_string]
     if len(count)!=0 and max(count)>1:
         return False
     return True
+
+    # Alternatively
+    #--------------------
+    # if len(string) == len(string.lower())
+    #       return True
+    # return False
+    
