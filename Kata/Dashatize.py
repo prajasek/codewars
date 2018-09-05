823e82fe82227b6e1318b953205a49e50ac7a987
Prasanth Raja Sekaran <prasanth.dps@gmail.com>
2018-09-04 20:47:23 -0400

    Update Dashatize.py

--- Kata/Dashatize.py
+++ Kata/Dashatize.py
@@ -3,14 +3,17 @@ def dashatize(num):
     # Given a number, return a string with dash'-'marks before
     # and after each odd integer, but do not begin or end 
     # the string with a dash mark.
+    # Example:
+    # 12434 should return "1-24-3-4"  
     #
     
-    output = []
+   
     if "None" in str(num):
         return "None"
     
-    for digit in str(num).strip("-"):
-        if int(digit)%2 != 0:   #odd number    
+    output = []
+    for digit in str(num).strip("-"):      # if number is negative (ex : -1), remove the '-' sign
+        if int(digit)%2 != 0:              # odd number    
             to_append = "-{}-".format(digit)
             output.append(to_append)
         else: 
