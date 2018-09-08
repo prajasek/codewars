0e8c091eae1e23f09b4f2d0157aa2326c10d661d
Prasanth <prasanth.meteora@gmail.com>
2018-09-08 09:52:21 -0400

    Update turle_race.py

---
+++ Kata/Turtle_race.py
@@ -0,0 +1,28 @@
+def race(v1, v2, g):
+    
+    # Two tortoises named A and B must run a race. 
+    # A starts with an average speed of 720 feet per hour. 
+    # B knows she runs faster than A. When she starts, 
+    # she can see that A has a 70 feet lead but B's speed 
+    # is 850 feet per hour. How long will it take B to catch A?
+    #
+    # v1 = speed of A (slower)
+    # v2 = speed of B (faster)
+    # g  = B's lead on A
+    #
+    # Equation:
+    # d/v1 = (d+g)/v2
+    # d*v2 = x*v1 + g*v1
+    # d = g*v1 / v2-v1
+    # time = x/v1 
+    
+    if v1>=v2 : return None
+   
+    distance = g*v1 / (v2-v1)
+    time = distance / v1
+    print (distance, time)
+    hours = int(distance // v1)
+    mins = int(time*60%60)
+    secs = int(time*3600%60)
+    print([hours,mins,secs])
+    return [hours,mins,secs]
