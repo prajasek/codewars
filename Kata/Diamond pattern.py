334bf8c6b2d8f984507634d9fed3b8b425c2bddb
Prasanth <prasanth.meteora@gmail.com>
2018-09-08 22:02:16 -0400

    add diamond_pattern.py

---
+++ Kata/Diamond pattern.py
@@ -0,0 +1,21 @@
+def diamond(n):
+
+	# creating diamond pattern (n = rows)
+	# n = 3 
+	# 
+	#  *
+	# ***
+	#  *
+
+
+	
+    if n%2==0 or n<=0:
+        return None
+    numbers_pattern = [i for i in range(n+1) if i%2==1]
+    reverse_pattern = [items for items in reversed(numbers_pattern[:-1])]
+    numbers_pattern = numbers_pattern + reverse_pattern
+    max_no = max(numbers_pattern)
+    pattern = [" "*((max_no-i)/2) + "*"*i + "\n" for i in numbers_pattern]
+    i = "\n".join(pattern)
+    print("".join(pattern))
+    return "".join(pattern)
