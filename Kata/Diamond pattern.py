75dab2d04e7ce5aabf57ef4803883cbbde4191cf
Prasanth Raja Sekaran <prasanth.dps@gmail.com>
2018-09-08 22:05:27 -0400

    Update Diamond pattern.py

--- Kata/Diamond pattern.py
+++ Kata/Diamond pattern.py
@@ -7,15 +7,12 @@ def diamond(n):
 	# ***
 	#  *
 
+    if n%2==0 or n<=0: return None
 
-	
-    if n%2==0 or n<=0:
-        return None
-    numbers_pattern = [i for i in range(n+1) if i%2==1]
-    reverse_pattern = [items for items in reversed(numbers_pattern[:-1])]
+    numbers_pattern = [i for i in range(n+1) if i%2==1]        				# [1,3,5,7...]
+    reverse_pattern = [items for items in reversed(numbers_pattern[:-1])]		
     numbers_pattern = numbers_pattern + reverse_pattern
     max_no = max(numbers_pattern)
     pattern = [" "*((max_no-i)/2) + "*"*i + "\n" for i in numbers_pattern]
-    i = "\n".join(pattern)
-    print("".join(pattern))
     return "".join(pattern)
+
