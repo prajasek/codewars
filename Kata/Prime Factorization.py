42dd4f82f55c05ef5f05183b35ffc23646c4c1bb
Prasanth Raja Sekaran <prasanth.dps@gmail.com>
2018-09-15 22:58:27 -0400

    added comments

--- Kata/Prime Factorization.py
+++ Kata/Prime Factorization.py
@@ -6,18 +6,20 @@ def primeFactors(n):
 	# Arrange the factorization from smallest to largest 
 	# Input:  20
 	# Output :  (2**2)(5)
-
-    prime = 2; largest = 2 ; num = n 
-
+	
+	
+    # Find largest prime factor 
+    prime = 2; largest_factor = 2 ; num = n 
     while num >= prime:   
         if num % prime == 0 :
-            largest = prime
+            largest_factor = prime
             num = num/prime
         else:   
             prime = prime+1
-
+    
+    # count factorization powers 
     prime_count = {}
-    for number in range(2, largest+1):
+    for number in range(2, largest_factor+1):
         count = 0
         while n%number ==0:
             count +=1
@@ -25,10 +27,7 @@ def primeFactors(n):
         if count>0:
             prime_count[number] = count 
     
-    print(prime_count)
-    
-
     primes_with_powers = ["({})".format(str(v) + "**" + str(pwr)) if pwr>1 else 
-    "({})".format(str(v)) for v, pwr in sorted(prime_count.items())]
+			  "({})".format(str(v)) for v, pwr in sorted(prime_count.items())]
 
     return "".join(primes_with_powers)
