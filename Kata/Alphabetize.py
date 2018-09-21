bb8cd5c26f80bdd1f3c8a7a5e939d229853c7d11
Prasanth Raja Sekaran <prasanth.dps@gmail.com>
2018-09-20 21:40:48 -0400

    added comment

--- Kata/Alphabetize.py
+++ Kata/Alphabetize.py
@@ -1,6 +1,11 @@
 def alphabet_position(text):
     
-    return_string = 
-	[str(ord(letter.lower())-96) for letter in text if 1<=ord(letter.lower())-96<=26]
+    # Replace each letter with their position in the alphabet
+    # if not an alphabet, then ignore
+    # 
+    # Input    : "abcdz"
+    # Output   :  "1 2 3 4 26"
+	
+    return_string = [str(ord(letter.lower())-96) for letter in text if 1<=ord(letter.lower())-96<=26]
     
     return " ".join(return_string)
