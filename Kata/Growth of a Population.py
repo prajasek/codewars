ac771e7930a2ac2433c3eeb61d2ec5daa343c5cd
Prasanth Raja Sekaran <prasanth.dps@gmail.com>
2020-02-28 07:52:04 -0500

    changed variable "year" to "number_of_years"
    Inline comment along "year" variable declaration removed.

--- Kata/Growth of a Population.py
+++ Kata/Growth of a Population.py
@@ -7,9 +7,9 @@ def nb_year(p0, percent, aug, p):
     # 'aug'     - increase/decrease in population per year (added or subtracted) 
     # 'p'       - Threshold of population to reach
     
-    year=0            # keep count of number of years
+    number_of_years = 0            
     while p0<p:
         p0 = p0 + p0*percent/100 + aug
-        year=year+1
-    return int(year)
+        number_of_years = number_of_years + 1
+    return int(number_of_years)
     
